ea9e5b6d37fac222196cf872fb965a677d3a95fb
Kelly Howell <worldwarwe_5@hotmail.com>
2021-02-01 20:55:02 -0600

    added new algo practice file

---
+++ algorithmsPractice.js
@@ -0,0 +1,16 @@
+//1. write a function that will print out all the nubmers from 1 to 255
+function Oneto155(){
+	for (var i = 0; i < 255; i++){
+		console.log(i);
+	}
+}
+//Oneto155();
+//2. Print odds from 1 to 1000
+function Oddsto1000(){
+	for (var i = 0; i <1000; i++){
+		if (i % 2 != 0){
+			console.log(i);
+		}
+	}
+}
+//Oddsto1000();
